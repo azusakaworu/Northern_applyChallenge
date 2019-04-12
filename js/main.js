67fd629b36f6b886c012a1b41eea0c84a457b89f
Ling Ding <azusakaworu@gmail.com>
2019-04-12 17:15:50 -0400

    box-sizing

--- js/main.js
+++ js/main.js
@@ -1,36 +1,45 @@
 let vm = new Vue({
 
-    el:'#app',
-    data:{
-        input:{email:"",choose:""}
+    el: '#app',
+    data: {
+        input: { email: "", choose: "" }
     },
 
 
-    methods:{
-        ajaxFun(){
+    methods: {
+
+        thankMSG() {
+            alert("hahaha");
+
+        },
+
+
+
+        ajaxFun() {
             console.log("trying to submit");
 
-             if(this.input.email != "" && this.input.choose != ""){
+            if (this.input.email != "" && this.input.choose != "") {
 
                 let url = `admin/form.php`;
-                fetch(url, {method: 'POST'})
-                .then(res => res.json())
-                .then(data => {
-                    if (data == "worng"){
-                        // if the php file returns a failure, try again
-                        console.log("authentication failed, try again");
-                        return;
-                    } else {
-                        // if the back-end authentication works, then go to the users page
-                        
-                    }
+                fetch(url, { method: 'POST' })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data == "worng") {
+                            // if the php file returns a failure, try again
+                            console.log("authentication failed, try again");
+                            return;
+                        } else {
+                            // if the back-end authentication works, then go to the users page
+                            thankMSG();
 
-                }) .catch(function(error) {
-                    console.error(error);
-                });
+                        }
+
+                    }).catch(function (error) {
+                        console.error(error);
+                    });
 
 
-                }
+            }
         }
 
     }
