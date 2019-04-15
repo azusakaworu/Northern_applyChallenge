424b4474795b16adf15b1f8633591500908759eb
Ling Ding <azusakaworu@gmail.com>
2019-04-15 19:25:44 -0400

    thankyou page did

--- js/main.js
+++ js/main.js
@@ -2,73 +2,55 @@ let vm = new Vue({
 
     el: '#app',
     data: {
-        email:"",
-        errors:"",
-        choose:""
+        email: "",
+        errors: "",
+        choose: "",
+        formSection:true,
+        ThanksSection:false
+
     },
 
 
     methods: {
-
-
         thankMSG() {
-            alert("hahaha");
-
+            //alert("hahaha");
+            this.formSection=false,
+            this.ThanksSection=true
         },
 
-        validEmail(email){
-            var emailReg =/^\w{3,}@[A-z]{2,6}\.[A-z]{2,5}$/;
+        validEmail(email) {
+            var emailReg = /^\w{3,}@[A-z]{2,6}\.[A-z]{2,5}$/;
             return emailReg.test(email);
-            
         },
 
-        checkForm(e){
+        checkForm(e) {
             e.preventDefault();
-           
+            console.log("trying to submit");
 
-             console.log("trying to submit");
-           
-             if (this.email != "" && this.choose != "") {
-                 let url = `./form.json`;
+            if (this.email != "" && this.choose != "") {
+                let url = `./form.json`;
                 fetch(url, { method: 'POST' })
                     .then(res => res.json())
                     .then(data => {
                         console.log(this.email);
                         if (!this.validEmail(this.email)) {
-                            // if the php file returns a failure, try again
                             // console.log("Please enter a valid email address. ");
                             this.errors = "Please enter a valid email address."
 
                             return;
                         } else {
-                            // if the back-end authentication works, then go to the users page
-                            // thankMSG();
+                            //works
                             console.log("you did!! tank you!!");
+                            //loading functio
+
+                            //tankyou page
                             this.thankMSG();
 
                         }
-
                     }).catch(function (error) {
                         console.error(error);
                     });
-
-
-           }
-     
-            
-
+            }
         }
-
-     
-
-
-
-        // ajaxFun(){
-           
-        // }
-
     }
-
-
-
 });
